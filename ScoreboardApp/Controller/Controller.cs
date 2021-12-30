4bb40b7ebc81df65479d570c2530e4efdf5abb3b
TheBad0men <58023162+TheBad0men@users.noreply.github.com>
2021-12-29 17:15:20 -0700

    MVC initialization.

---
+++ ScoreboardApp/Controller/Controller.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Controller
+{
+    public class Controller
+    {
+
+    }
+}
---
+++ ScoreboardApp/Model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public int Score { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
